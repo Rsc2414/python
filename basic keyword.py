ca027a2c5402f26f7a963b79590ad7b5bf4e2d48
Rsc2414 <143814425+Rsc2414@users.noreply.github.com>
2023-09-20 10:35:43 +0530

    Add files via upload

---
+++ basic keyword.py
@@ -0,0 +1,99 @@
+s="my ,name,is ,rishabh ,singh ,chauhan"
+ss="hello"
+
+
+# for all char of string in upper case
+# s=s.upper()
+# print(s)
+
+
+# for all char of string in lower case
+# s=s.lower()
+# print(s)
+
+
+# for check all char of string in upper case
+# s=s.isupper()
+# print(s)
+
+
+# for all  char of string in upper case
+# s=s.islower()
+# print(s)
+
+
+# for first char of string in upper case
+# s=s.capitalize()
+# print(s)
+
+
+# finding index of  "r" char from right most of the string
+# s=s.rfind('r')
+# print(s)
+
+
+# for count "r" char how many time present in string 
+# s=s.count('a',2,20)
+# print(s)
+
+
+# finding index of "r" char from the string
+# s=s.find('r')
+# print(s)
+
+
+# finding index of "r" char from the string
+# s=s.index('r')
+# print(s)
+
+
+# for spliting string from ","
+# s=s.split(',')
+# print(s)
+
+
+# for spliting string from "," for 3 time left
+# s=s.split(",",3)
+# print(s)
+
+
+# for spliting string from "," for 3 time right
+# s=s.rsplit(",",3)
+# print(s)
+
+
+# joining string by ","
+# s=','.join(s)
+# print(s)
+
+
+# s=s.strip()
+# print(s)
+
+
+# s=s.lstrip()
+# print(s)
+
+
+# s=ss.rstrip()
+# print(s)
+
+
+# s=ss.ljust(10,",")
+# print(s)
+
+
+# s=ss.rjust(10,",")
+# print(s)
+
+
+# =================================================================================================
+# checking is string is starting with "my"
+# s=s.startswith("my")
+# print(s)
+
+
+# checking is string is ending with "chauhan"
+# s=s.endswith("chauhan")
+# print(s)
+
